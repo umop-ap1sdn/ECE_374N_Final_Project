a5d7109b6fe6debd1dcf3546e475a0e6e465a396
Caleb Devon <cmdevon2003@gmail.com>
2025-04-23 23:42:59 -0500

    add timeout rate

--- n_back_acc.py
+++ n_back_acc.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from pyxdf import load_xdf
 
 def calc_acc_dope(sub, day, time, dataset_base='data/Group 12'):
@@ -6,11 +7,13 @@ def calc_acc_dope(sub, day, time, dataset_base='data/Group 12'):
     
     accuracies = []
     dopes = []
+    timeout_rate = []
 
     for file in os.listdir(path):
         streams, headers = load_xdf(os.path.join(path, file))
         trials = 0
         correct = 0
+        timeouts = 0
         dope = 1
         start_timestamp = 0
 
@@ -25,28 +28,47 @@ def calc_acc_dope(sub, day, time, dataset_base='data/Group 12'):
             
             if marker[0] == 1.0:
                 correct += 1
-                dope *= 1.0 / ((marker[1] - start_timestamp) * 1)
+                dope *= 1.0 / (((marker[1] - 0.25) - start_timestamp) * 1)
 
-            if marker[0] == 2.0:
-                dope *= 0.01 / ((marker[1] - start_timestamp) * 1)
+            if marker[0] == 2.0 or marker[0] == 300.0:
+                dope *= 0.01 / (((marker[1] - 0.25) - start_timestamp) * 1)
+
+            if marker[0] == 300.0:
+                timeouts += 1
+
+            
+
         
         # print(trials)
         accuracies.append(correct / trials)
         dopes.append(float(dope ** (1.0 / trials)))
+        timeout_rate.append(float(timeouts / trials))
     
-    return accuracies, dopes
+    return accuracies, dopes, timeout_rate
 
 if __name__ == '__main__':
     times = ['pre', 'post']
+    z = 1.960
     for i in range(1, 5):
         print(f'Subject 122{i}:')
         for j in range(1, 3):
             print(f'Day {j}:')
             for time in times:
                 print(f'{time} N-Back')
-                acc, dope = calc_acc_dope(i, j, time)
-                print(f'N-Back Accuracies: {acc}')
-                print(f'N-Back Dope Scores: {dope}')
+                acc, dope, timeout = calc_acc_dope(i, j, time)
+                acc_avg = np.mean(acc)
+                acc_ci = z * np.std(acc) / np.sqrt(5.0)
+
+                dope_avg = np.mean(dope)
+                dope_ci = z * np.std(dope) / np.sqrt(5.0)
+
+                timeout_avg = np.mean(timeout)
+                timeout_ci = z * np.std(timeout) / np.sqrt(5.0)
+
+                print(f'N-Back Accuracies: {acc}, Avg: {acc_avg:.3f} +- {acc_ci:.3f}')
+                print(f'N-Back Dope Scores: {dope}, Avg: {dope_avg:.3f} +- {dope_ci:.3f}')
+                print(f'N-Back Timeout Rates: {timeout}, Avg: {timeout_avg:.3f} +- {timeout_ci:.3f}')
+                
             
         print()
         
